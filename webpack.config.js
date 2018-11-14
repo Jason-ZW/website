f68b442bcad06cf27bd11f37e867aa63ac4847c8
Jason-ZW <zhenyang@rancher.com>
2018-11-14 23:05:30 +0800

    Add index page

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader',
-                include: /flexboxgrid/
+                loader: 'style-loader!css-loader'
             },
             {
                 test: /\.(png|jpg)$/,
@@ -61,6 +60,14 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 loader: 'file-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
             },
             {
                 test: /\.json$/,
