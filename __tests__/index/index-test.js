b625dd551a05b8fee548355f354a6c9d5f0248c7
Jason-ZW <zhenyang@rancher.com>
2018-11-20 16:17:11 +0800

    Configure jest test framework

--- __tests__/index/index-test.js
+++ __tests__/index/index-test.js
@@ -1,10 +1,18 @@
+'use strict';
 import expect from 'expect';
 import React from 'react';
-import EntryPoint from '../../src/index/index';
+import { TopBar, Header } from '../../src/index/header';
 import renderer from 'react-test-renderer';
 
-const entrypoint = renderer.create(
-    <EntryPoint />,
-);
-let tree = entrypoint.toJSON();
-expect(tree).toMatchSnapshot();
+describe('renders correctly', () => {
+    it('topbar correctly', () => {
+        const tree = renderer.create(<TopBar />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it('header correctly', () => {
+        const tree = renderer.create(<Header />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+
--- src/index/index.js
+++ src/index/index.js
@@ -3,101 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Row, Col, Carousel, Button } from 'react-bootstrap';
+import { TopBar, Header } from './header';
 import 'bootstrap/scss/bootstrap.scss';
 import "font-awesome/scss/font-awesome.scss";
 import '../../static/css/index.scss';
-
-class TopBar extends React.Component {
-    displayName: 'TopBar';
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <div className="top-bar">
-                <div className="container">
-                    <Row>
-                        <Col sm={8} md={8}>
-                            <div className="top-bar-left">
-                                <span>
-                                    <i className="fa fa-envelope-o"/> supporter@3-nice.com
-                                </span>
-                            </div>
-                        </Col>
-                        <Col className="d-none d-md-block d-sm-block text-right">
-                            <div>
-                                <span className="top-bar-span-first">
-                                    <a className="fa fa-facebook" href="https://www.facebook.com/" title="Facebook" target="_blank"/>
-                                </span>
-                                <span className="top-bar-span">
-                                    <a className="fa fa-google-plus" href="https://plus.google.com/" title="Google" target="_blank"/>
-                                </span>
-                                <span className="top-bar-span">
-                                    <a className="fa fa-twitter" href="https://twitter.com/" title="Twitter" target="_blank"/>
-                                </span>
-                                <span className="top-bar-span">
-                                    <a className="fa fa-linkedin" href="https://www.linkedin.com/" title="Linkedin" target="_blank"/>
-                                </span>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-            </div>
-        )
-    }
-}
-
-class Header extends React.Component {
-    displayName: 'Header';
-    constructor() {
-        super();
-    }
-    render() {
-        return (
-            <div className="carousel-container">
-                <Row className="carousel-row">
-                    <Col md={12}>
-                        <Carousel>
-                            <Carousel.Item>
-                                <div className="carousel-mask"/>
-                                <div
-                                    className="d-block w-100 c-1"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Professional Laptop Repairs </h3>
-                                    <p>Specialized in Laptop Service, We working hard to build a reputation of customer satisfaction through technical excellence and friendly staff.</p>
-                                    <button type="button" className="btn btn-primary">Learn More</button>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="carousel-mask"/>
-                                <div
-                                    className="d-block w-100 c-2"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Second slide label</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    <button type="button" className="btn btn-primary">Learn More</button>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="carousel-mask"/>
-                                <div
-                                    className="d-block w-100 c-3"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Third slide label</h3>
-                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                    <button type="button" className="btn btn-primary">Learn More</button>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        </Carousel>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
-}
 
 class EntryPoint extends React.Component {
     displayName: 'EntryPoint';
